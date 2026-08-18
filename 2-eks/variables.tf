ba614f5212dec1b818574341c3ffdc9d2d3daa28
Rohit <79014667+Rohitdevops73@users.noreply.github.com>
2026-08-19 03:53:58 +0530

    adding infrastructure related files

---
+++ 2-eks/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  type    = string
+  default = "itkannadigaru"
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = "1.30"
+}
+
+variable "ssh_key_name" {
+  type        = string
+  description = "Name of the EC2 key pair for SSH access to worker nodes"
+  default     = "itkannadigaru"
+}
